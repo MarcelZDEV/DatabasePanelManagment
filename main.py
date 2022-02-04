870f04f089a57c93cdc050b60685bbc6fadda92e
MarcelZDEV <marcelowatyz@gmail.com>
2022-02-04 12:04:11 +0100

    login.jinja2 and register.jinja2 is work now

--- main.py
+++ main.py
@@ -14,9 +14,32 @@ def account():
     return render_template("account.jinja2")
 
 
-@app.route("/account-login")
+@app.route("/account-login", methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        user = request.form["name"]
+        password = request.form["pass"]
+        print(user, password)
     return render_template('login.jinja2')
+
+
+@app.route('/account-register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        user_reg = request.form["name_reg"]
+        password_reg = request.form["pass_reg"]
+        email = request.form["email_reg"]
+        sqlq = "SELECT COUNT(1) FROM users WHERE username = %s"
+        val = (user_reg, )
+        cursor.execute(sqlq, val)
+        if cursor.fetchone()[0]:
+            return render_template('login.jinja2')
+        else:
+            execute = "INSERT INTO users (username, password, email, account) VALUES(%s, %s, %s, %s)"
+            value = (user_reg, password_reg, email, "normal")
+            cursor.execute(execute, value)
+            db.commit()
+    return render_template('register.jinja2')
 
 
 if __name__ == '__main__':
