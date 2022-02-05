7d51bf80f84c25292f886536e16bec537ffb2066
MarcelZDEV <marcelowatyz@gmail.com>
2022-02-05 14:22:26 +0100

    add new connects to db some new small updates

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, url_for, redirect, request, render_template, session
+
+import db
 from db import *
 from datetime import timedelta
 
@@ -35,8 +37,8 @@ def login():
         session.permanent = True
         user = request.form["name"]
         password = request.form["pass"]
-        sqlq = "SELECT COUNT(1) FROM users WHERE username = %s AND password = %s"
-        check_root = "SELECT account FROM users WHERE username = %s AND password = %s"
+        sqlq = "SELECT COUNT(1) FROM users_db WHERE username = %s AND password = %s"
+        check_root = "SELECT account FROM users_db WHERE username = %s AND password = %s"
         val = (user, password)
         cursor.execute(sqlq, val)
         if cursor.fetchone():
@@ -63,13 +65,13 @@ def register():
         user_reg = request.form["name_reg"]
         password_reg = request.form["pass_reg"]
         email = request.form["email_reg"]
-        sqlq = "SELECT COUNT(1) FROM users WHERE username = %s"
+        sqlq = "SELECT COUNT(1) FROM users_db WHERE username = %s"
         val = (user_reg,)
         cursor.execute(sqlq, val)
         if cursor.fetchone()[0]:
             return render_template('login.jinja2')
         else:
-            execute = "INSERT INTO users (username, password, email, account) VALUES(%s, %s, %s, %s)"
+            execute = "INSERT INTO users_db (username, password, email, account) VALUES(%s, %s, %s, %s)"
             value = (user_reg, password_reg, email, "normal")
             cursor.execute(execute, value)
             db.commit()
@@ -83,9 +85,29 @@ def logout():
     return render_template('account.jinja2')
 
 
-@app.route('/Databases')
+@app.route('/Databases', methods=['POST', 'GET'])
 def databases():
-    return render_template('database_home.jinja2')
+    if "admin" in session:
+        root = session["admin"]
+        return render_template('database_home.jinja2', root=root)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/Connect', methods=['POST', 'GET'])
+def connect():
+    if request.method == 'POST':
+        if 'admin' in session:
+            host_connect = request.form['host_connect']
+            user_connect = request.form['name_connect']
+            pass_connect = request.form['pass_connect']
+            data_connect = request.form['data_connect']
+            user_name = session["admin"]
+            query_connect = "INSERT INTO connects_db (host, username, password, database_name, user_name_table) VALUES (%s, %s, %s, %s, %s)"
+            value_connect = (host_connect, user_connect, pass_connect, data_connect, user_name)
+            cursor.execute(query_connect, value_connect)
+        db.commit()
+    return render_template('connect_db.jinja2')
 
 
 if __name__ == '__main__':
